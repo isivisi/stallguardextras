96d1b06d9f65b893634d699433c692fb11001658
John <jiannandrea@gmail.com>
2023-04-29 23:05:00 -0400

    wip

--- stallguardextras.py
+++ stallguardextras.py
@@ -4,10 +4,39 @@ from . import print_stats
 class StallGuardExtras:
     def __init__(self, config):
         self.printer = config.get_printer()
-        self.stallguard = self.printer.load_object(config, 'stallguard')
+        self.stallGuard = self.printer.load_object(config, 'stallguard')
+        self.loop  = None
+        self.kin = self.printer.lookup_object('toolhead').get_kinematics()
+        self.kinSteppers = self.kin.get_steppers() # grab all steppers defined by the kinamatics
 
-        self.printer.register_event_handler("toolhead:set_position". self.movement)
-        self.printer.register_event_handler("toolhead:manual_move", self.movement)
+        self.drivers = self.printer.lookup_objects(module="tmc")
+
+        self.printer.register_event_handler("klippy:connect", self.onKlippyConnect)
+
+        gcode = self.printer.lookup_object("gcode")
+        gcode.register_command("ENABLE_STALLGUARD_CHECKS", self.disableChecks, desc="")
+        gcode.register_command("DISABLE_STALLGUARD_CHECKS", self.disableChecks, desc="")
+
+    def onKlippyConnect():
+        enableChecks()
+
+    def enableChecks():
+        if (self.loop == None):
+            reactor = self.printer.get_reactor()
+            curTime = reactor.monotonic()
+            self.loop = reactor.register_timer(self.doChecks, curTime + 1)
+
+    def disableChecks():
+        if (self.loop != None):
+            self.printer.get_reactor().unregister_timer(self.loop)
+            self.loop = None
     
-    def movement(self):
-        
+    def doChecks(self):
+        # diag_pin
+
+        # SGTHRS
+        # SG_RESULT
+
+        self.mcu_tmc.get_register('SG_RESULT')
+
+        return self.printer.get_reactor().NEVER

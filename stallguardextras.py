06abfda644003e380449fc689cc5c700d65ece03
John <jiannandrea@gmail.com>
2023-04-30 11:50:29 -0400

    wip

--- stallguardextras.py
+++ stallguardextras.py
@@ -17,7 +17,7 @@ class StallGuardExtras:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.updateTime = float(config.get("update_time", 0.1))
-        self.crashThreshold = int(config.get("crash_threshold", 35))
+        self.crashThreshold = int(config.get("crash_threshold", 600))
         self.loop = None
 
         self.drivers = {}
@@ -27,8 +27,14 @@ class StallGuardExtras:
 
         for name, obj in self.printer.lookup_objects():
             if any(driver.lower() in name.lower() for driver in self.supportedDrivers):
+                driverDetails = {
+                    'driver': None,
+                    'history': None
+                }
                 if ('extruder' in name): self.extruders.append(obj)
-                else: self.drivers[name.split(" ")[1]] = obj
+                else: 
+                    driverDetails["driver"] = obj
+                    self.drivers[name.split(" ")[1]] = driverDetails
                 logging.info("found stallguard compatible driver " + str(obj))
 
         self.printer.register_event_handler("klippy:connect", self.onKlippyConnect)
@@ -46,7 +52,7 @@ class StallGuardExtras:
         #gcmd.respond_info(str(self.drivers))
         responses = {}
         for name in self.drivers:
-            responses[name] = int(self.drivers[name].mcu_tmc.get_register('SG_RESULT'))
+            responses[name] = int(self.drivers[name]["driver"].mcu_tmc.get_register('SG_RESULT'))
         gcmd.respond_info(str(responses))
 
     def enableChecks(self):
@@ -67,14 +73,29 @@ class StallGuardExtras:
         # SG_RESULT // get result
         
         # driver
-        for driver in self.drivers:
-            result = int(self.drivers[driver].mcu_tmc.get_register('SG_RESULT'))
-            # logging.info("driver %s value %s" % (driver, str(result)))
+        for d in self.drivers:
+            driverInfo = self.drivers[d]
+
+            result = int(driverInfo["driver"].mcu_tmc.get_register('SG_RESULT'))
+
+            if (driverInfo["history"] == None): driverInfo["history"] = result
+
+            if (result > 400): 
+                logging.info("driver %s value %s, current %s" % (d, str(result), 0))
+
             # raise self.printer.command_error()
 
-            if (result > self.crashThreshold):
-                self.printer.invoke_shutdown("Shutdown due to stallguard on driver %s" % (driver,))
+            difference = result - driverInfo["history"]
 
+            # todo: use (current / expected current) to influence the crashThreshold
+
+            if (difference > self.crashThreshold):
+                logging.info("Detecting motor slip, transient peaked %s for motor %s, %s more than previous value" % (str(result), d, str(difference)))
+                self.printer.invoke_shutdown("Detecting motor slip, transient peaked %s for motor %s, %s more than previous value" % (str(result), d, str(difference)))
+                #self.printer.invoke_shutdown("Shutdown due to stallguard on driver %s." % (d,str(result)))
+
+            driverInfo["history"] = result
+        
         return eventtime + self.updateTime
 
 def load_config(config):

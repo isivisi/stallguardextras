a1af4a0562f0f513bf773134ebabaa473021417d
John <jiannandrea@gmail.com>
2023-04-30 01:32:40 -0400

    able to grab drivers directly

--- stallguardextras.py
+++ stallguardextras.py
@@ -1,14 +1,31 @@
+# A utility class to test resonances of the printer
+#
+# Copyright (C) 2023 John Iannandrea    <jiannandrea@gmail.com>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+
 import logging
 
 class StallGuardExtras:
+    supportedDrivers = [
+        'TMC2209'
+    ]
+    
     def __init__(self, config):
         self.printer = config.get_printer()
         self.updateTime = float(config.get("update_time", 1.0))
         self.loop = None
-        #self.kin = self.printer.lookup_object('toolhead').get_kinematics()
-        #self.kinSteppers = self.kin.get_steppers() # grab all steppers defined by the kinamatics
 
-        self.drivers = self.printer.lookup_objects(module="TMC2209")
+        self.drivers = {}
+        self.extruders = []
+
+        logging.info("--- Stall guard extras loaded ---")
+
+        for name, obj in self.printer.lookup_objects():
+            if any(driver.lower() in name.lower() for driver in self.supportedDrivers):
+                if ('extruder' in name): self.extruders.append(obj)
+                else: self.drivers[name.split(" ")[1]] = obj
+                logging.info("found stallguard compatible driver " + str(obj))
 
         #self.printer.register_event_handler("klippy:connect", self.onKlippyConnect)
 
@@ -21,9 +38,12 @@ class StallGuardExtras:
         self.enableChecks()
 
     def debug(self, gcmd):
-        for driver in self.drivers:
-            gcmd.respond_info(vars(driver))
-            #gcmd.respond_info("driver %s value %s" % (driver.name, driver.mcu_tmc.get_register('SG_RESULT')))
+        #gcmd.respond_info(str(self.printer.lookup_objects()))
+        #gcmd.respond_info(str(self.drivers))
+        responses = {}
+        for name in self.drivers:
+            responses[name] = self.drivers[name].mcu_tmc.get_register('SG_RESULT')
+        gcmd.respond_info(str(responses))
 
     def enableChecks(self):
         if (self.loop == None):
@@ -44,7 +64,9 @@ class StallGuardExtras:
         
         # driver
         for driver in self.drivers:
-            driver.mcu_tmc.get_register('SG_RESULT')
+            result = self.drivers[driver].mcu_tmc.get_register('SG_RESULT')
+            # logging.warning()
+            # raise self.printer.command_error()
 
         return self.printer.get_reactor().NEVER
 

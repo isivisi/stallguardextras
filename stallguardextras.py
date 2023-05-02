f93d314f9488a694dc33858f7e2658c7bd345284
John <jiannandrea@gmail.com>
2023-05-02 13:07:45 -0400

    wip refactor

--- stallguardextras.py
+++ stallguardextras.py
@@ -7,6 +7,67 @@
 import logging, chelper
 #from motion_report import DumpTrapQ
 
+class DriverHelper:
+    def __init__(self, name, driver, stepper, printer):
+        self.name = name
+        self.printer = printer
+        self.driver = driver
+        self.stepper = stepper
+        self.history = None
+        self.expectedRange = 25
+        self.triggers = 0
+        self.expectedPos = 0
+        self.trapq = None # trapezoidal velocity queue for stepper
+        self.lastVelocity = 0
+
+        self.deviationTolerance = 25
+
+    def check(self, eventtime, updateTime):
+        last_move = self.getLastMove(eventtime)
+        velocity = last_move.start_v + last_move.accel if last_move else 0
+
+        result = int(self..driver.mcu_tmc.get_register('SG_RESULT'))
+
+        if (self.history == None): self.history = result
+
+        if (velocity != self.lastVelocity): self.expectedPos = result
+
+        difference = self.expectedPos - result
+
+        expectedDropRange = self.lerp(driverInfo.expectedRange, self.deviationTolerance, updateTime * 0.5)
+        if (self.lastVelocity != velocity): expectedDropRange = self.deviationTolerance * 2
+            
+        if (difference > expectedDropRange):
+            self.triggers += 1
+            if (driverInfo.triggers > 10):
+                self.printer.invoke_shutdown("Detecting motor slip on motor %s. %s value deviated by %s from previous. maximum %s deviation" % (d,str(result),str(difference), str(expectedDropRange)))
+        else:
+            self.triggers = max(0, self.triggers - 1)
+        self.history = result
+        self.expectedRange = expectedDropRange
+
+        self.lastVelocity = velocity
+
+    def getStatus(self):
+        return {
+            "sg_result": self.history,
+            "sg_expected": self.expectedPos,
+            "sg_triggers": self.triggers,
+        }
+
+    def getLastMove(self, eventtime):
+        print_time = self.printer.lookup_object('mcu').estimated_print_time(eventtime)
+        ffi_main, ffi_lib = chelper.get_ffi()
+        data = ffi_main.new('struct pull_move[1]') # make a new move struct
+        count = ffi_lib.trapq_extract_old(self.trapq, data, 1, 0., print_time) # fill move struct with last value in steppers movement queue
+        if not count:
+            return None
+        # start_v, move_t, print_time, accel, start_x, start_y, start_z x_r, y_r, z_r
+        return data[0]
+
+    def driverHasField(self, field):
+        return field in self.driver.fields.field_to_register.keys()
+
 class StallGuardExtras:
     supportedDrivers = [
         'TMC2130',
@@ -55,20 +116,10 @@ class StallGuardExtras:
             if any(driver.lower() in name.lower() for driver in self.supportedDrivers):
                 stepper = [s for s in steppers if s.get_name() == name.split(" ")[1]]
                 logging.info("%s found %s" % (name, str(stepper)))
-                driverDetails = {
-                    'driver': None,
-                    'history': None,
-                    'expectedRange': 25,
-                    'triggers': 0,
-                    'expectedPos': 0,
-                    'type': name.split(" ")[0],
-                    'trapq': stepper[0].get_trapq() if len(stepper) else None, # Trapezoidal velocity queue for stepper
-                }
                 if ('extruder' in name):
                     self.extruders.append(obj)
                 else:
-                    driverDetails["driver"] = obj
-                    self.drivers[name.split(" ")[1]] = driverDetails
+                    self.drivers[name.split(" ")[1]] = DriverHelper(name, obj, stepper, printer)
                 logging.info("found stallguard compatible driver " + str(obj))
 
     def onMotorOff(self, eventtime):
@@ -85,16 +136,13 @@ class StallGuardExtras:
         self.enableChecks()
     
     def setupDrivers(self):
-        for d in self.drivers:
-            driver = self.drivers[d]["driver"];
+        for name, driverDef in self.drivers:
+            driver = driverDef.driver
             # set drivers stallguard strength if specified
-            if (self.sgthrs != None and self.driverHasField(driver, 'sgthrs')):
+            if (self.sgthrs != None and driverDef.driverHasField(driver, 'sgthrs')):
                 driver.fields.set_field("sgthrs", int(self.sgthrs))
-            elif (self.sgt != None and self.driverHasField(driver, 'sgt')):
+            elif (self.sgt != None and driverDef.driverHasField(driver, 'sgt')):
                 driver.fields.set_field("sgt", int(self.sgt))
-    
-    def driverHasField(self, driver, field):
-        return field in driver.fields.field_to_register.keys()
     
     # move the toolhead safely within printing speeds to finetune the stallguard settings
     def tune(self, gcmd):
@@ -141,66 +189,22 @@ class StallGuardExtras:
     def debugMove(self, gcmd):
         move = self.getLastMove(self.printer.get_reactor().monotonic())
         gcmd.respond_info("start_v:%s, move_t:%s, accel:%s\nx_r:%s x_y:%s x_z:%s" % (str(move.start_v), str(move.move_t), str(move.accel), str(move.x_r), str(move.y_r), str(move.z_r)))
-
-    def getLastMove(self, stepperInfo, eventtime):
-        print_time = self.printer.lookup_object('mcu').estimated_print_time(eventtime)
-        ffi_main, ffi_lib = chelper.get_ffi()
-        data = ffi_main.new('struct pull_move[1]') # make a new move struct
-        count = ffi_lib.trapq_extract_old(stepperInfo["trapq"], data, 1, 0., print_time) # fill move struct with last value in steppers movement queue
-        if not count:
-            return None
-        # start_v, move_t, print_time, accel, start_x, start_y, start_z x_r, y_r, z_r
-        return data[0]
     
     lastVelocity = 0
     def doChecks(self, eventtime):
-        # diag_pin
-
-        # SGTHRS // set threshold
-        # SG_RESULT // get result 0 - 510
 
         # TODO: cleanup, force rehome on x and y when slipping, cant do anything with z though :(
-        # TODO: deal with big transient spikes being set as expected, forcing failure
         
         # driver
-        for d in self.drivers:
-            driverInfo = self.drivers[d]
+        for name, driver in self.drivers:
+            driver.check(eventtime, self.updateTime):
 
-            last_move = self.getLastMove(driverInfo, eventtime)
-            velocity = last_move.start_v + last_move.accel if last_move else 0
-
-            result = int(driverInfo["driver"].mcu_tmc.get_register('SG_RESULT'))
-
-            if (driverInfo["history"] == None): driverInfo["history"] = result
-
-            if (velocity != self.lastVelocity): driverInfo["expectedPos"] = result
-
-            difference = driverInfo["expectedPos"] - result
-
-            expectedDropRange = self.lerp(driverInfo["expectedRange"], self.deviationTolerance, self.updateTime * 0.5)
-            if (self.lastVelocity != velocity): expectedDropRange = self.deviationTolerance * 2
-            
-            if (difference > expectedDropRange):
-                driverInfo["triggers"] += 1
-                if (driverInfo["triggers"] > 10):
-                    self.printer.invoke_shutdown("Detecting motor slip on motor %s. %s value deviated by %s from previous. maximum %s deviation" % (d,str(result),str(difference), str(expectedDropRange)))
-            else:
-                driverInfo["triggers"] = max(0, driverInfo["triggers"] - 1)
-            driverInfo["history"] = result
-            driverInfo["expectedRange"] = expectedDropRange
-
-        self.lastVelocity = velocity
-        
         return eventtime + self.updateTime
 
     def get_status(self, eventtime):
         data = {}
         for d in self.drivers:
-            data[d] = {
-                "sg_result": self.drivers[d]["history"],
-                "sg_expected": self.drivers[d]["expectedPos"],
-                "sg_triggers": self.drivers[d]["triggers"],
-            }
+            data[d] = self.drivers[d].getStatus()
         return data
 
 def load_config(config):

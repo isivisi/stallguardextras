153fa8972748f747465a894798cc41b4038c8390
John I <jiannandrea@gmail.com>
2023-05-06 22:15:17 -0400

    Changes (#1)
    * wip
    
    * wip
    
    * accurate but slow to detect
    
    * wip
    
    * config changes
    
    * wip
    
    * better but too many triggers
    
    * better
    
    * removed during homing as endstop not easly detectable
    
    * working with move check
    
    * first and last clock are the same, so move ends cant be detected that way, BUT it can with microsteps so we should be good with both
    
    * working, doesn't work with homing as it sends many small move commands
    
    * fits the klipper 0.250 update limit better I tihnk
    
    * wip
    
    * wip, need to save all moves in a buffer so we can lookback and not loose movements
    
    * wip

--- readme.md
+++ readme.md
@@ -8,10 +8,17 @@ This has only been tested on TMC2209 drivers
 
 ## Config
 ```
+# load stallguardextras.py from ~/klipper/klippy/extras
 [stallguardextras]
-update_time: 0.01 # time in seconds to query the drivers
+
+[collision_detection]
+update_time: 0.01       # time in seconds to query the drivers
+deviation_tolerance: 50
 # sgthrs: 250
 # sgt: 0 #
+
+[extruder_detection]
+#WIP
 ```
 
 ## Moonraker Auto Update
--- stallguardextras.py
+++ stallguardextras.py
@@ -16,6 +16,26 @@ supportedDrivers = [
 def lerp(start, end, delta):
     return (start + (end - start) * delta)
 
+# copy of c struct data
+class MoveHelper:
+    def __init__(self, rawMove):
+        self.first_clock = rawMove.first_clock
+        self.last_clock = rawMove.last_clock
+        self.start_position = rawMove.start_position
+        self.step_count = rawMove.step_count
+        self.interval = rawMove.interval
+        self.add = rawMove.add
+
+    def getStatus(self):
+        return {
+            "first_clock": self.first_clock,
+            "last_clock": self.last_clock,
+            "start_position": self.start_position,
+            "step_count": self.step_count,
+            "interval": self.interval,
+            "add": self.add,
+        }
+
 class DriverHelper:
     def __init__(self, sg, name, driver, stepper):
         self.name = name
@@ -32,12 +52,29 @@ class DriverHelper:
         self.lastStepPos = 0
         self.smoothedResult = 0
 
-        self.moving = False
-        self.lastMicroStep = 0
+        #self.enableChecks = bool(sg.config.getsection(self.name).get('check_collisions', True))
 
-        self.waitForChange = True
+        self.moving = False
+        self.stepsMoving = False
+        self.lastMicroStep = 0
+        self.lastMove = None
+        
+        # move queue we have yet to process
+        self.moveQueue = []
+
+        self.hasChanged = True
 
         self.deviationTolerance = sg.deviationTolerance
+
+    
+    def onEnabled(self):
+        self.moving = False
+        self.stepsMoving = False
+        self.lastMove = None
+        self.lastMicroStep = -1
+        self.expectedPos = 0
+        self.hasChanged = True
+        self.history = None
 
     # Perform some checks using the stallguard result to determine if the motor is 
     # slipping / about to slip.
@@ -49,124 +86,172 @@ class DriverHelper:
     # the motor will change momentum / change to a different move. Then grab the sg value,
     # wait to see if it deviates and assume stalled if it does.
     #
+    # Then we wait for a driver command to be picked up to make sure deviation was expected
+    # or not.
+    #                                  |----| 0.250 second max
+    #   sg_value_deviated: - - - - - - X--OK - - -
+    #   move_sent_to_drv : - - - - - - - -X - - -
+    #
     # works great except I need to find a better way to determine when the specifc motor is
     # to change momentum. sometimes the motor changes to a different, but consistent
     # value and its not picking up on that.
-    def check(self, eventtime, updateTime):
+    def check(self, eventtime, updateTime, onDetect):
         status = self.driver.get_status()
         standStillIndicator = False
         if (status['drv_status']): standStillIndicator = status['drv_status'].get('stst', False)
-
-        movingChangedThisTick = False
-
-        last_move = self.getLastMove(eventtime)
-        velocity = last_move.start_v + last_move.accel if last_move else 0
-        steppos = self.stepper.get_commanded_position()
-
         result = int(self.driver.mcu_tmc.get_register('SG_RESULT'))
-        microstepcounter = int(self.driver.mcu_tmc.get_register('MSCNT')) # 0 to 1023
 
-        # determine if we've moved since last check
-        if (microstepcounter != self.lastMicroStep and not self.moving):
-            movingChangedThisTick = True
-            self.moving = True
-        elif (microstepcounter == self.lastMicroStep and self.moving):
-            movingChangedThisTick = True
-            self.moving = False
-        elif (microstepcounter < self.lastMicroStep):
-            movingChangedThisTick = abs((microstepcounter + 1023) - self.lastMicrostep) > 500
-        else:
-            movingChangedThisTick = abs(microstepcounter - self.lastMicrostep) > 500
-            
-        
         if (standStillIndicator):
-            self.triggers = 0
             self.expectedPos = result
+            self.triggers = 0
+            self.hasChanged = False
 
         if (self.history == None): self.history = result
 
-        if (movingChangedThisTick or self.lastVelocity != velocity or steppos != self.lastStepPos): #(movingChangedThisTick or self.lastVelocity != velocity or steppos != self.lastStepPos):
-            self.triggers = 0
-            self.expectedPos = result
-            #self.expectedRange = self.deviationTolerance * 2
+        # movement changed
+        changedThisTick = self.hasMovementChanged(eventtime)
+        self.hasChanged = True if changedThisTick else self.hasChanged
 
         difference = self.expectedPos - result
         expectedDropRange = lerp(self.expectedRange, self.deviationTolerance, updateTime * 0.5)
             
-        if (difference > expectedDropRange):
-            self.triggers += 1
-            
-            if (self.triggers <= 2):
-                logging.warning("detecting slip, adjusting expected pos from %s to %s incase anomaly" % (str(self.expectedPos),str(lerp(self.expectedPos, result, 0.5))))
-                #self.expectedPos = self.expectedPos - (difference*1.2) #lerp(self.expectedPos, result, 0.5) # give it a chance to readjust incase of drastic change duing normal ops
-            if (self.triggers > 2):
-                #if self.sg.testMode: logging.warning("Detecting motor slip on motor %s. %s value deviated by %s from previous. maximum %s deviation" % (self.name,str(result),str(difference), str(expectedDropRange)))
-                self.printer.invoke_shutdown("Detecting motor slip on motor %s. %s value deviated by %s from previous. maximum %s deviation" % (self.name,str(result),str(difference), str(expectedDropRange)))
+        if (abs(difference) > expectedDropRange):
+            self.triggers += updateTime
+
+            #if (self.triggers <= 1):
+            #    logging.warning("detecting slip, adjusting expected pos from %s to %s incase anomaly" % (str(self.expectedPos),str(lerp(self.expectedPos, result, 0.5))))
+            #    self.expectedPos = self.expectedPos - difference #lerp(self.expectedPos, result, 0.5) # give it a chance to readjust incase of drastic change duing normal ops
+            if (self.triggers > 0.250):
+                if (not self.hasChanged):
+                    onDetect(self, result, difference)
+                else:
+                    logging.warning("%s slip ignored, intended change" % (self.name,))
+                    self.expectedPos = result
+                    self.triggers = 0
+                    self.hasChanged = False
         else:
-            self.triggers = max(0, self.triggers - updateTime)
+            self.triggers = max(-0.250 if self.hasChanged else 0, self.triggers - updateTime)
+
+        if (self.triggers <= -0.250):
+            logging.warning("%s expected change detected" % (self.name,))
+            self.triggers = 0
+            self.hasChanged = False
+
         self.history = result
         self.expectedRange = expectedDropRange
 
-        self.lastVelocity = velocity
-        self.lastStepPos = steppos
+    def hasMovementChanged(self, eventtime):
+        movingChangedThisTick = False
+
+        steppos = self.lastStepPos
+        steppos = self.stepper.get_commanded_position()
+        #movingChangedThisTick = steppos != self.lastStepPos
+
+        microstepcounter = int(self.driver.mcu_tmc.get_register('MSCNT')) # 0 to 1023
+
+        # testing new thing
+        mcu = self.stepper.get_mcu()
+        self.moveQueue += self.getMoves(eventtime-0.05, eventtime)
+        clock = mcu.print_time_to_clock(mcu.estimated_print_time(eventtime))
+        move = None
+        if (move and self.lastMove):
+            if (move.first_clock != self.lastMove.first_clock and not self.moving):
+                movingChangedThisTick = True
+                self.moving = True
+                if (len(self.moveQueue)): self.lastMove = self.moveQueue.pop()
+                logging.warning("move started")
+            elif (clock > self.lastMove.last_clock and self.moving):
+                movingChangedThisTick = True
+                self.moving = False
+                logging.warning("move ended")
+                if (move.first_clock > self.lastMove.first_clock):
+                    if (len(self.moveQueue)): self.lastMove = self.moveQueue.pop()
+                    self.moving = True
+                    logging.warning("move continued")
+        else:
+            if (len(self.moveQueue)): self.lastMove = self.moveQueue.pop()
+
+        if (microstepcounter != self.lastMicroStep and not self.stepsMoving):
+            logging.warning("movement started")
+            movingChangedThisTick = True
+            self.stepsMoving = True
+        elif (microstepcounter == self.lastMicroStep and self.stepsMoving):
+            logging.warning("movement stopped")
+            movingChangedThisTick = True
+            self.stepsMoving = False
+            
+        #self.lastVelocity = velocity
+        #self.lastStepPos = steppos
         self.lastMicroStep = microstepcounter
+        self.lastKnownPrintTime = clock
+        #self.lastMove = move
+
+        return movingChangedThisTick
 
     def getStatus(self):
         return {
             "sg_result": self.history,
             "sg_expected": self.expectedPos,
             "sg_triggers": self.triggers,
+            "latest_moves": [move.getStatus() for move in self.moveQueue]
         }
 
     # grab specifically just the start_position of the most recent move we are sending to the mcu
-    def getStartPosOfLastMove(self, eventtime):
-        if (not self.stepper): return
+    def getMoves(self, start, end):
+        if (not self.stepper): return None
+        startClock = clock = self.stepper.get_mcu().print_time_to_clock(start)
+        clock = self.stepper.get_mcu().print_time_to_clock(end)
         # first_clock, last_clock, start_position, step_count, interval, add
-        steps, count = self.stepper.dump_steps(1, eventtime, eventtime)
-        return steps[count].start_position
-
-    def getLastMove(self, eventtime):
-        print_time = self.printer.lookup_object('mcu').estimated_print_time(eventtime)
-        ffi_main, ffi_lib = chelper.get_ffi()
-        data = ffi_main.new('struct pull_move[1]') # make a new move struct
-        count = ffi_lib.trapq_extract_old(self.trapq, data, 1, 0., print_time) # fill move struct with last value in steppers movement queue
-        if not count:
-            return None
-        # start_v, move_t, print_time, accel, start_x, start_y, start_z x_r, y_r, z_r
-        return data[0]
+        steps, count = self.stepper.dump_steps(1, startClock, clock)
+        if not steps: return []
+        returnSteps = []
+        for i in range(count):
+            returnSteps.append(MoveHelper(steps[i]))
+        return returnSteps
 
     def driverHasField(self, field):
         return field in self.driver.fields.field_to_register.keys()
 
-class StallGuardExtras:
+class CollisionDetection:
     def __init__(self, config):
+        if (not config):
+            return
+        self.config = config
         self.printer = config.get_printer()
-        self.updateTime = float(config.get("update_time", 0.1))
+        self.updateTime = float(config.get("update_time", 0.125))
+        self.disableOnHome = bool(config.get("disable_on_home", True))
         self.sgthrs = config.get("sgthrs", None)
         self.sgt = config.get("sgt", None)
         self.testMode = bool(config.get("test_mode", False))
-        self.deviationTolerance = int(config.get("deviation_tolerance", 100))
+        self.deviationTolerance = int(config.get("deviation_tolerance", 50))
         self.loop = None
 
         self.drivers = {}
         self.extruders = []            
 
+        self.faultOnDetection = True
+
         self.printer.register_event_handler("klippy:connect", self.onKlippyConnect)
 
         self.printer.register_event_handler("stepper_enable:motor_off", self.onMotorOff)
-        self.printer.register_event_handler("homing:homing_move_begin", self.onMotorOn)
+        
+        if (self.disableOnHome):
+            self.printer.register_event_handler("homing:homing_move_begin", self.onHomingOn)
+            self.printer.register_event_handler("homing:homing_move_end", self.onHomingOff)
+            self.printer.register_event_handler("homing:home_rails_begin", self.onHomingOn)
+            self.printer.register_event_handler("homing:home_rails_end", self.onHomingOff)
 
         gcode = self.printer.lookup_object("gcode")
         gcode.register_command("ENABLE_STALLGUARD_CHECKS", self.enableChecks, desc="")
         gcode.register_command("DISABLE_STALLGUARD_CHECKS", self.disableChecks, desc="")
         gcode.register_command("TUNE_STALLGUARD", self.tune, desc="")
         gcode.register_command("DEBUG_STALLGUARD", self.debug, desc="")
-        
-        #extruderSensing = ExtruderDetection(config.getsection('extruder_detection'))
+
+        self.printer.add_object("collision_detection", self)
 
     def onKlippyConnect(self):
         #self.setupDrivers()
-        #self.enableChecks()
+        self.enableChecks()
 
         toolhead = self.printer.lookup_object("toolhead")
         kin = self.printer.lookup_object("toolhead").get_kinematics()
@@ -193,15 +278,14 @@ class StallGuardExtras:
     def onMotorOff(self, eventtime):
         self.disableChecks()
 
-        #with open("stallout.csv", "w") as txt_file:
-        #    for line in self.csv:
-        #        txt_file.write(line + "\n")
-        
-        #self.csv = []
-
-    def onMotorOn(self, eventtime):
-        self.setupDrivers()
-        self.enableChecks()
+    def onHomingOn(self, *args):
+        #self.disableChecks()
+        self.faultOnDetection = False
+    
+    def onHomingOff(self, *args):
+        #self.setupDrivers()
+        #self.enableChecks()
+        self.faultOnDetection = True
     
     def setupDrivers(self):
         for name, driverDef in self.drivers.items():
@@ -227,7 +311,7 @@ class StallGuardExtras:
 
         # SAVE_CONFIG
         configfile = self.printer.lookup_object('configfile')
-        configfile.set('stallguardextras', 'sgthrs', '0')
+        configfile.set('collision_detection', 'sgthrs', '0')
 
     def queryObjects(self, gcmd):
         gcmd.respond_info(str(self.printer.lookup_objects()))
@@ -246,6 +330,8 @@ class StallGuardExtras:
 
     def enableChecks(self, gcmd=None):
         if (self.loop == None):
+            #for name, driver in self.drivers.items():
+                #driver.onEnabled()
             reactor = self.printer.get_reactor()
             curTime = reactor.monotonic()
             self.loop = reactor.register_timer(self.doChecks, curTime + self.updateTime)
@@ -259,16 +345,20 @@ class StallGuardExtras:
         move = self.getLastMove(self.printer.get_reactor().monotonic())
         gcmd.respond_info("start_v:%s, move_t:%s, accel:%s\nx_r:%s x_y:%s x_z:%s" % (str(move.start_v), str(move.move_t), str(move.accel), str(move.x_r), str(move.y_r), str(move.z_r)))
     
-    lastVelocity = 0
     def doChecks(self, eventtime):
 
         # TODO: cleanup, force rehome on x and y when slipping, cant do anything with z though :(
         
         # driver
         for name, driver in self.drivers.items():
-            driver.check(eventtime, self.updateTime)
+            driver.check(eventtime, self.updateTime, self.onDetect)
 
         return eventtime + self.updateTime
+
+    # event when deviation is detected and no move command is detected
+    def onDetect(self, driver, value, diff):
+        if (self.faultOnDetection):
+            self.printer.invoke_shutdown("Detecting motor slip on motor %s. %s value deviated by %s from previous. maximum %s deviation" % (driver.name,str(value),str(diff), str(self.deviationTolerance)))
 
     def get_status(self, eventtime):
         data = {}
@@ -278,10 +368,17 @@ class StallGuardExtras:
 
 class ExtruderDetection:
     def __init__(self, config):
+        if (not config):
+            return
         self.printer = config.get_printer()
         self.updateTime = float(config.get("update_time", 0.1))
         self.jamDetectEnabled = bool(config.get("jam_detect", False))
         self.runoutDetect = bool(config.get("runout_detect", False))
+
+class StallGuardExtras:
+    def __init__(self, config):
+        self.collisionDection = CollisionDetection(config.getsection('collision_detection'))
+        self.extruderSensing = ExtruderDetection(config.getsection('extruder_detection'))
 
 def load_config(config):
     return StallGuardExtras(config)
